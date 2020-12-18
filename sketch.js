2b7fb6509974185de00659d30b56faaa3418fd00
prathiksha1516 <71827736+prathiksha1516@users.noreply.github.com>
2020-12-18 16:32:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//Create variables here
+var dog
+var dogImage,dogImage1
+
+function preload()
+{
+  //load images here
+  dogImage. loadImge ="images/dogImag.png"
+  dogImage. loadImge ="images/dogImag1.png"
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
